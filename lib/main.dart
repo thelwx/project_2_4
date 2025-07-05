3734815c3dbacfddcd77484d7408100e2f254ca0
LWX <lingwx-wm21@student.tarc.edu.my>
2025-07-05 17:23:23 +0800

    BMI Calculator

--- lib/main.dart
+++ lib/main.dart
@@ -131,6 +131,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        // This is changes
       ),
       body: Center(
         child: Padding(
